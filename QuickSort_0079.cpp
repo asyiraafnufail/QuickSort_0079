b6ec2df467bc0c2ad0cb657b4eaa99709c96b8e0
asyiraafnufail <asyiraaf4@gmail.com>
2024-03-29 13:52:20 +0700

    menuliskan cout untuk array elemen pada bagian void input

--- QuickSort_0079.cpp
+++ QuickSort_0079.cpp
@@ -9,7 +9,17 @@ int n;
 void input() {
     while (true)
     {
-        cout << "Masukkan panjang elemen array = "
+        cout << "Masukkan panjang elemen array = ";
         cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
     }
+
+    cout << "\n--------------------" << endl;
+    cout << "\nEnter Array Element!" << endl;
+    cout << "\n--------------------" << endl;
+    
 }
