9be485e63cabd0b41fccf27f7c85bca0fc984925
asyiraafnufail <asyiraaf4@gmail.com>
2024-03-29 14:11:25 +0700

    menuliskan fungsi while 3

--- QuickSort_0079.cpp
+++ QuickSort_0079.cpp
@@ -56,5 +56,13 @@ void q_short (int low, int high)
             i++;
             cmp_count++;
         }
+        cmp_count++;
+
+        while ((arr[j] > pivot) && (j >= low))
+        {
+            j--;
+            cmp_count++;
+        }
+        cmp_count++;
     }
 }
